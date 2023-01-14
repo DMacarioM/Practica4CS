854a15050544d584740183bc3f7a4c727d9abd80
DMacarioM <99421448+DMacarioM@users.noreply.github.com>
2023-01-14 16:15:27 +0100

    Proyecto importado

---
+++ Practica4/src/main/java/com/Practica4/calidad/Practica4/App.java
@@ -0,0 +1,13 @@
+package com.Practica4.calidad.Practica4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
